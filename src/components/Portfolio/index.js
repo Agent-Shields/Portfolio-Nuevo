e93270a16b07c08ff27ea6919d22cfb226b8cdc8
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-25 15:19:04 -0500

    Passing project specifically through props, now each project renders on my portfolio! need to fix styling and the images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import Project from "../Project"
+import Outfitters from '../../assets/portfolioPieces/Outfitters.PNG'
+import TimedQuiz from '../../assets/portfolioPieces/QuizProof.PNG'
+import TechBlog from '../../assets/portfolioPieces/Tech-Blog.PNG'
+import RandomPass from '../../assets/portfolioPieces/RandomPassProof.jpg'
+import ConcertScout from '../../assets/portfolioPieces/Concert-Scout.png'
+import BudgetTracker from '../../assets/portfolioPieces/Go-Budget-Tracker.PNG'
 
 function Portfolio() {
     return (
@@ -12,17 +18,17 @@ function Portfolio() {
                 </div>
             </div>
             <div className="row mx-2">
-                <Project />
-                <Project />
-            </div>
-            {/* <div className="row mx-2">
-                <Project />
-                <Project />
+                <Project name="Outfitters" image={Outfitters} gitHub="https://github.com/hmailahn/outfitters" deployed="https://outfitters-agent-shields.herokuapp.com/"/>
+                <Project name="Concert Scout" image={ConcertScout} gitHub="https://github.com/austinslatey/concert-scout"  deployed="https://austinslatey.github.io/concert-scout/" />
             </div>
             <div className="row mx-2">
-                <Project />
-                <Project />
-            </div> */}
+                <Project name="Tech-Blog" image={TechBlog} gitHub="https://github.com/Agent-Shields/Tech-Blog"  deployed="https://tech-blog-agent-shields.herokuapp.com/" />
+                <Project name="Random Password Generator" image={RandomPass} gitHub="https://github.com/Agent-Shields/Random-Pass"  deployed="https://agent-shields.github.io/Random-Pass/" />
+            </div>
+            <div className="row mx-2">
+                <Project name="Budget Tracker" image={BudgetTracker} gitHub="https://github.com/Agent-Shields/Go-Budget-Tracker"  deployed="https://shields-go-budget-tracker.herokuapp.com/" />
+                <Project name="Timed Quiz" image={TimedQuiz} gitHub="https://github.com/Agent-Shields/Timed-Quiz"  deployed="https://agent-shields.github.io/Timed-Quiz/" />
+            </div>
         </section>
     )
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,22 +1,16 @@
 import React from 'react'
-import Outfitters from '../../assets/portfolioPieces/Outfitters.PNG'
+
 
 function Project(props) {
-
-    const currentProj = {
-        name: "Outfitters",
-        gitHub: "https://github.com/hmailahn/outfitters",
-        deployed: "https://outfitters-agent-shields.herokuapp.com/",
-    }
 
     return (
         <div className='col-md-6 img-thumbnail' style={{
             minHeight: "200px",
             maxHeight: "200px",
-            backgroundImage: `url(${Outfitters})`,
+            backgroundImage: `url(${props.image})`,
             backgroundSize: "contain"
         }}>
-                <h1>{currentProj.name}</h1>
+                <h1>{props.name}</h1>
         </div>
     )
 }
