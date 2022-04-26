dc11bec696776dd6337d441f9c9c60a3a04febac
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-25 23:15:50 -0500

    toggle on mouseEnter/Leave for projects is working, added css to handle cover / show toggle

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 
 function Project(props) {
-    
+
     const [isHovering, setisHovering] = useState(false)
 
     function handleMouseEnter(e) {
@@ -16,15 +16,23 @@ function Project(props) {
     }
 
     return (
-        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`col-md-6 img-thumbnail img-responsive cursor-pointer ${isHovering ? "" : "hidden"}`} style={{
-            minHeight: "200px",
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='col-md-6 img-thumbnail img-responsive' style={{
             maxHeight: "200px",
+            minHeight: "200px",
+            backgroundSize: "cover",
             backgroundImage: `url(${props.image})`,
-            backgroundSize: "cover"
         }}>
+            <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`${isHovering ? "show" : "hidden"}`} style={{
+                opacity:'75',
+                maxHeight: "200px",
+                minHeight: "200px",
+                backgroundColor:"darkgray",
+                backgroundSize: "cover"
+            }}>
                 <h1 className='row mx-2' >{props.name}</h1>
                 <a href={props.gitHub} alt="GitHub Repository">GitHub Link</a>
                 <a href={props.deployed} alt="Deployed Application">Deployed Application</a>
+            </div>
         </div>
     )
 }
