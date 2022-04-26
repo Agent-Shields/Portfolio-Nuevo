5098943ce194a2eb029f290cb9029a4a136fa7e3
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-25 22:26:55 -0500

    experimenting with mouseEnter mouseLeave

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function Project(props) {
+    
+    const [isHovering, setisHovering] = useState(false)
+
+    function handleMouseEnter(e) {
+        setisHovering(true)
+        console.log("enter")
+    }
+
+    function handleMouseLeave() {
+        setisHovering(false)
+        console.log('leave')
+    }
 
     return (
-        <div className='col-md-6 img-thumbnail img-responsive' style={{
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`col-md-6 img-thumbnail img-responsive cursor-pointer ${isHovering ? "" : "hidden"}`} style={{
             minHeight: "200px",
             maxHeight: "200px",
             backgroundImage: `url(${props.image})`,
-            backgroundSize: "cover",
-            "&:hover": {
-                background: "#efefef"
-            }
+            backgroundSize: "cover"
         }}>
                 <h1 className='row mx-2' >{props.name}</h1>
                 <a href={props.gitHub} alt="GitHub Repository">GitHub Link</a>
