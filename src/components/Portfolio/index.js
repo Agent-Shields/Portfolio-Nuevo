990e4e660d95296c7116a0e12c0563aec332a92e
Johnathan Shields <93488468+Agent-Shields@users.noreply.github.com>
2022-05-05 16:39:04 -0500

    updating portfolio to include final group project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,7 +3,7 @@ import Project from "../Project"
 import Outfitters from '../../assets/portfolioPieces/Outfitters.PNG'
 import TimedQuiz from '../../assets/portfolioPieces/QuizProof.PNG'
 import NoteTaker from '../../assets/portfolioPieces/NoteTaker.PNG'
-import RandomPass from '../../assets/portfolioPieces/RandomPassProof.jpg'
+import RecipEase from '../../assets/portfolioPieces/RecipEase.PNG'
 import ConcertScout from '../../assets/portfolioPieces/Concert-Scout.png'
 import BudgetTracker from '../../assets/portfolioPieces/Go-Budget-Tracker.PNG'
 
@@ -22,8 +22,8 @@ function Portfolio() {
                 <Project name="Concert Scout" image={ConcertScout} gitHub="https://github.com/austinslatey/concert-scout"  deployed="https://austinslatey.github.io/concert-scout/" />
             </div>
             <div className="row mx-2">
+                <Project name="RecipEase" image={RecipEase} gitHub="https://github.com/austinslatey/RecipEase"  deployed="http://thebestrecipease.herokuapp.com/" />
                 <Project name="Note Taker" image={NoteTaker} gitHub="https://github.com/Agent-Shields/Note-Taker"  deployed="https://note-taker-agent-shields.herokuapp.com/" />
-                <Project name="Random Password Generator" image={RandomPass} gitHub="https://github.com/Agent-Shields/Random-Pass"  deployed="https://agent-shields.github.io/Random-Pass/" />
             </div>
             <div className="row mx-2">
                 <Project name="Budget Tracker" image={BudgetTracker} gitHub="https://github.com/Agent-Shields/Go-Budget-Tracker"  deployed="https://shields-go-budget-tracker.herokuapp.com/" />
